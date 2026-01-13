64c69106070f32f580395eb57a53eb07e76b271e
Benjy <benjymuchaga@gmail.com>
2026-01-13 22:02:15 +0200

    Refine JobRole model with hourly and overtime handling

--- src/Model/jobRole.java
+++ src/Model/jobRole.java
@@ -1,26 +1,65 @@
 package Model;
 
-public class jobRole {
+public class JobRole {
 
-    private int roleId;
+    
+    private int jobRoleId;
+    private String companyName;
     private String roleName;
     private String department;
     private double hourlyRate;
     private double overtimeRate;
 
-    public jobRole() {}
+    public JobRole() {}
 
-    public jobRole(int roleId, String roleName, String department, double hourlyRate,double overtimeRate) {
-        this.roleId = roleId;
+    public JobRole(int jobRoleId, String companyName, String roleName, String department, double hourlyRate, double overtimeRate) {
+        this.jobRoleId = jobRoleId;
+        this.companyName = companyName;
         this.roleName = roleName;
         this.department = department;
         this.hourlyRate = hourlyRate;
-        this.overtimeRate=overtimeRate;
+        this.overtimeRate = overtimeRate;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+    
+
+    public void setJobRoleId(int jobRoleId) {
+        this.jobRoleId = jobRoleId;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setHourlyRate(double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public void setOvertimeRate(double overtimeRate) {
+        this.overtimeRate = overtimeRate;
+    }
+
+    public JobRole(int jobRoleId, String roleName, String department, double hourlyRate,double overtimeRate) {
+        this.jobRoleId =jobRoleId;
+        this.roleName = roleName;
+        this.department = department;
+       
     }
 
 
-    public int getRoleId() {
-        return roleId;
+    public int getJobRoleId() {
+        return jobRoleId;
     }
     public String getRoleName() {
         return roleName;
